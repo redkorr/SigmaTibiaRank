696d65dab5ca438c295dab23f69bbbd52926f2ec
senbar <sendek.bartosz@gmail.com>
2020-08-31 00:35:40 +0200

    work on decoding jsons in progress

--- Backend/CharacterService.hs
+++ Backend/CharacterService.hs
@@ -3,7 +3,9 @@ module CharacterService where
 
 import Domain
 import Network.HTTP.Simple
+import Data.Aeson (decode)
 getVonvir:: ()-> IO (Maybe Character)
 getVonvir () = do 
-    response <- httpLBS "https://api.tibiadata.com/v2/characters/vonvir.json"
-    return $ Just $ Character "vonvir" 1
+    response <-  httpLBS "https://api.tibiadata.com/v2/characters/vonvir.json"
+    let character= decode $ getResponseBody response :: Maybe Character 
+    return character
