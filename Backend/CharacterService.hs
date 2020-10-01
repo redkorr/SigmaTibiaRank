5a66640d857b3ff411078e2a40a60aa2a54da776
senbar <sendek.bartosz@gmail.com>
2020-10-01 23:32:33 +0200

    parsing some data finally works

--- Backend/CharacterService.hs
+++ Backend/CharacterService.hs
@@ -3,14 +3,35 @@ module CharacterService where
 
 import Domain
 import Network.HTTP.Simple (parseRequest,  getResponseBody, httpLBS )
-import Data.Aeson (decode)
+import Data.Aeson (decode, eitherDecode, Object, (.:))
+import Data.Map (Map, (!))
+import Debug.Trace (trace)
+import Data.Aeson.Types 
+import qualified Data.ByteString.Lazy as LB
 
 getCharacter:: String-> IO (Maybe Character)
 getCharacter nickname = do 
     initRequest <- parseRequest $ createApiLink nickname
     response <-  httpLBS $ initRequest
-    let character= decode $ getResponseBody response :: Maybe Character 
-    return character
+    let responseBody =  processBody $ getResponseBody response
+    let character= decode $ trace ("haskell jest pojebany" ++ show responseBody) responseBody :: Maybe ( Character) 
+    return $ character 
+
+processBody :: LB.ByteString -> LB.ByteString
+processBody body = trace ( "ja prdle: " ++ show (doShit body)) body
+
+doShit:: LB.ByteString -> Either String String 
+doShit body = do 
+    object <- eitherDecode body
+    let parser = (\obj -> do
+            characters <- obj .: "characters"
+            mdata <- characters.: "data"
+            name <- mdata .: "name"
+            return name)
+    parseEither parser object
+    
+            
+
 
 createApiLink:: String -> String
 createApiLink nickname = getConnectionString () ++ (replaceSpaceWithPlus nickname) ++ ".json"
